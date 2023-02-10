0d279f5971c04c8a6dec37918d58040e3f153539
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-02-10 19:53:56 +0530

    Updated the contracts

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -8,24 +8,62 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
+  mapping ( address => uint256 ) public balances;
+  uint256 public constant threshold = 1 ether;
+
+  event Stake (address indexed staker, uint256 indexed amount);
+  address[] public stakers;
+
+  uint256 public deadline = block.timestamp + 144 hours;
+  bool public openForWithdraw;
+
   constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() public payable {
+    balances[msg.sender] += msg.value;
+    stakers.push(msg.sender);
+    emit Stake(msg.sender, msg.value);
+    console.log();
+  }
 
 
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+  function execute() public {
+    if(address(this).balance > threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+    } else {
+      openForWithdraw = true;
+    }
+  }
 
 
   // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
+  function withdraw() public {
+    if(openForWithdraw == true) {
+      (bool success,) = msg.sender.call{value: balances[msg.sender]}("");
+      require(success, "Transfer failed!");
+      openForWithdraw = false;
+    }
+  }
 
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    } else {
+      return deadline - block.timestamp;
+    }
+  }
 
 
   // Add the `receive()` special function that receives eth and calls stake()
-
+  receive() external payable {
+    stake();
+  }
 }
